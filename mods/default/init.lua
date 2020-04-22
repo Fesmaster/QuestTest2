b6e071d4aa4e0dd2ef30aad82d0632b9ebd1a77b
Fesmaster <fessmk@gmail.com>
2020-04-22 12:54:43 -0400

    Basics Done

---
+++ mods/default/init.lua
@@ -0,0 +1,69 @@
+-- default (Minetest 0.4 mod)
+-- Most default stuff
+
+-- The API documentation in here was moved into doc/lua_api.txt
+
+WATER_ALPHA = 160
+WATER_VISC = 1
+LAVA_VISC = 7
+LIGHT_MAX = 14
+
+-- Definitions made by this mod that other mods can use too
+default = {}
+
+-- Load other files
+dofile(minetest.get_modpath("default").."/mapgen.lua")
+
+-- Set a noticeable inventory formspec for players
+minetest.register_on_joinplayer(function(player)
+	local cb = function(player)
+		minetest.chat_send_player(player:get_player_name(), "This is the [minimal] \"Minimal Development Test\" game. Use [minetest_game] for the real thing.")
+	end
+	minetest.after(2.0, cb, player)
+end)
+
+--
+-- Tool definition
+--
+
+-- The hand
+minetest.register_item(":", {
+	type = "none",
+	wield_image = "wieldhand.png",
+	wield_scale = {x=1,y=1,z=2.5},
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level = 0,
+		groupcaps = {
+			fleshy = {times={[2]=2.00, [3]=1.00}, uses=0, maxlevel=1},
+			crumbly = {times={[2]=3.00, [3]=0.70}, uses=0, maxlevel=1},
+			snappy = {times={[3]=0.40}, uses=0, maxlevel=1},
+			oddly_breakable_by_hand = {times={[1]=7.00,[2]=4.00,[3]=1.40}, uses=0, maxlevel=3},
+		},
+		damage_groups = {fleshy=1},
+	}
+})
+
+
+
+-- Default node sounds
+
+function default.node_sound_defaults(table)
+	table = table or {}
+	table.footstep = table.footstep or
+			{name="", gain=1.0}
+	table.dug = table.dug or
+			{name="default_dug_node", gain=1.0}
+	return table
+end
+
+-- Register nodes
+
+minetest.register_node("default:default", {
+	description = "Default Node",
+	tiles ={"default.png"},
+	groups = {oddly_breakable_by_hand=3},
+	sounds = default.node_sound_defaults(),
+})
+
+
---
+++ mods/default/mapgen.lua
@@ -0,0 +1,60 @@
+--
+-- Aliases for map generator outputs
+--
+
+
+minetest.register_alias("mapgen_stone", "default:default")
+minetest.register_alias("mapgen_dirt", "default:default")
+minetest.register_alias("mapgen_dirt_with_grass", "default:default")
+minetest.register_alias("mapgen_sand", "default:default")
+minetest.register_alias("mapgen_water_source", "air")
+minetest.register_alias("mapgen_river_water_source", "air")
+minetest.register_alias("mapgen_lava_source", "air")
+minetest.register_alias("mapgen_gravel", "default:default")
+
+minetest.register_alias("mapgen_tree", "default:default")
+minetest.register_alias("mapgen_leaves", "default:default")
+minetest.register_alias("mapgen_apple", "default:default")
+minetest.register_alias("mapgen_junglegrass", "default:default")
+
+minetest.register_alias("mapgen_cobble", "default:default")
+minetest.register_alias("mapgen_stair_cobble", "stairs:default")
+minetest.register_alias("mapgen_mossycobble", "default:default")
+
+minetest.clear_registered_biomes()
+minetest.clear_registered_decorations()
+
+minetest.register_biome({
+	name = "default:default",
+	--node_dust = "",
+	node_top = "default:default",
+	depth_top = 1,
+	node_filler = "default:default",
+	depth_filler = 1,
+	--node_stone = "",
+	--node_water_top = "",
+	--depth_water_top = ,
+	--node_water = "",
+	y_min = 5,
+	y_max = 31000,
+	heat_point = 50,
+	humidity_point = 50,
+})
+
+minetest.register_biome({
+	name = "default:grassland_ocean",
+	--node_dust = "",
+	node_top = "default:default",
+	depth_top = 1,
+	node_filler = "default:default",
+	depth_filler = 2,
+	--node_stone = "",
+	--node_water_top = "",
+	--depth_water_top = ,
+	--node_water = "",
+	y_min = -31000,
+	y_max = 4,
+	heat_point = 50,
+	humidity_point = 50,
+})
+
