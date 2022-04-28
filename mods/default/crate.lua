ae50bd6cb24619528e480816b14ad3154d0a587c
shadowshrig <tim@kellyinv.com>
2022-04-28 07:56:01 -0400

    6 crates more coming

--- mods/default/crate.lua
+++ mods/default/crate.lua
@@ -1,7 +1,7 @@
 
-qts.register_chest("default:crate", {
-	description = "Crate",
-	tiles = {"default_crate.png"},
+qts.register_chest("default:crate_oak", {
+	description = "Oak Wood Crate",
+	tiles = {"default_crate_oak.png"},
 	groups = {choppy = 2, oddly_breakable_by_hand = 2},
 	paramtype = "light",
 	paramtype2 = "facedir",
@@ -11,6 +11,72 @@ qts.register_chest("default:crate", {
 	invsize = 8*4,
 	get_chest_formspec = qtcore.get_default_chest_formspec,
 })
+
+qts.register_chest("default:crate_apple", {
+	description = "Apple Wood Crate",
+	tiles = {"default_crate_apple.png"},
+	groups = {choppy = 2, oddly_breakable_by_hand = 2},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	is_ground_content = false,
+	sounds = qtcore.node_sound_wood(),
+	
+	invsize = 8*4,
+	get_chest_formspec = qtcore.get_default_chest_formspec,
+})
+
+qts.register_chest("default:crate_coffee", {
+	description = "Coffee Wood Crate",
+	tiles = {"default_crate_coffee.png"},
+	groups = {choppy = 2, oddly_breakable_by_hand = 2},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	is_ground_content = false,
+	sounds = qtcore.node_sound_wood(),
+	
+	invsize = 8*4,
+	get_chest_formspec = qtcore.get_default_chest_formspec,
+})
+
+qts.register_chest("default:crate_aspen", {
+	description = "Aspen Wood Crate",
+	tiles = {"default_crate_aspen.png"},
+	groups = {choppy = 2, oddly_breakable_by_hand = 2},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	is_ground_content = false,
+	sounds = qtcore.node_sound_wood(),
+	
+	invsize = 8*4,
+	get_chest_formspec = qtcore.get_default_chest_formspec,
+})
+
+qts.register_chest("default:crate_mahogany", {
+	description = "Mahogany Wood Crate",
+	tiles = {"default_crate_mahogany.png"},
+	groups = {choppy = 2, oddly_breakable_by_hand = 2},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	is_ground_content = false,
+	sounds = qtcore.node_sound_wood(),
+	
+	invsize = 8*4,
+	get_chest_formspec = qtcore.get_default_chest_formspec,
+})
+
+qts.register_chest("default:crate_laternfruit", {
+	description = "Lantern Tree Wood Crate",
+	tiles = {"default_crate_lanternfruit.png"},
+	groups = {choppy = 2, oddly_breakable_by_hand = 2},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	is_ground_content = false,
+	sounds = qtcore.node_sound_wood(),
+	
+	invsize = 8*4,
+	get_chest_formspec = qtcore.get_default_chest_formspec,
+})
+
 
 --[[
 --A Test chest to see how well different inv sizes would work
