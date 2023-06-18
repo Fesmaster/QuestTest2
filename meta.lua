41af709c83699abb300634b0e5e3c9b90110f79b
Fesmaster <21015354+Fesmaster@users.noreply.github.com>
2023-06-18 16:42:38 -0500

    Spawn point selection (#40)
    * qts spawnpoint API
    * Obelisk Spwanpoint setter added

--- meta.lua
+++ meta.lua
@@ -142,7 +142,7 @@ local ObjectRef = {
 
     ---set number of health points
     ---@param hp number
-    ---@param reason HPChangeReason
+    ---@param reason? HPChangeReason
     set_hp = function(self, hp, reason) end, -- 
     
     ---returns an `InvRef` for players, otherwise returns `nil`
@@ -732,7 +732,7 @@ vector ={
  
     ---Create vector from string
     ---@param s string format: "(x, y, z)"
-    ---@param init number optional offset into string
+    ---@param init? number optional offset into string
     ---@return Vector|nil , number
     from_string = function(s, init) end, --
  
@@ -907,9 +907,10 @@ function math.round(x) end
 ---splits a string
 ---@param str string the string to split
 ---@param seperator string the string to split on
----@param include_empty boolean defaults to false. To include emptry strings 
----@param max_splits integer max splits to make
----@param sep_is_pattern boolean defaults to false. Set to true to make the seperator treated as regex
+---@param include_empty? boolean defaults to false. To include emptry strings 
+---@param max_splits? integer max splits to make
+---@param sep_is_pattern? boolean defaults to false. Set to true to make the seperator treated as regex
+---@return string[]
 function string.split(str, seperator, include_empty, max_splits, sep_is_pattern) end
 
 ---trims the whitespaces off of the start and end of a string
--- mods/qts/chatcommands.lua
+++ mods/qts/chatcommands.lua
@@ -21,4 +21,46 @@ minetest.register_chatcommand("devmode", {
 	end
 })
 
+minetest.register_chatcommand("spawnpoint", {
+	params="[<offset>]",
+	description = "Set your spawnpoint",
+	privs={creative=1},
+	func=function (name, param)
+		local offset = vector.new(0,0,0)
+		---@type Player
+		local player = minetest.get_player_by_name(name)
+		if player == nil then return end
+		local abspos = player:get_pos()
+		if param and param ~= "" then
+			local fields = string.split(param, ",", false)
+			if #fields ~= 3 then
+				minetest.chat_send_player(name, "Unable to complete spawnpoint set. Offset String format invalid. use 'x,y,z' format, where each can be a number for absolute or a number preceded by '~' for relative")
+				return
+			end
+			local map = {"x","y","z"}
+			for i, str in ipairs(fields) do
+				if string.sub(str, 1,1) == "~" then
+					local off = tonumber(string.sub(str, 2))
+					if off then
+						offset[map[i]] = off
+					else
+						minetest.chat_send_player(name, "Unable to complete spawnpoint set. Offset String format invalid. use 'x,y,z' format, where each can be a number for absolute or a number preceded by '~' for relative")
+						return
+					end
+				else
+					local abs = tonumber(str)
+					if abs then
+						abspos[map[i]] = abs
+					else
+						minetest.chat_send_player(name, "Unable to complete spawnpoint set. Offset String format invalid. use 'x,y,z' format, where each can be a number for absolute or a number preceded by '~' for relative")
+						return
+					end
+				end
+			end
+		end
 
+		--set the spawnpoint
+		qts.set_player_spawnpoint(player, abspos+offset)
+		minetest.chat_send_player(name, "spawnpoint set to: " .. (abspos+offset):to_string())
+	end
+})
